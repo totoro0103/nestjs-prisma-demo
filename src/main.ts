243469e713adf0b9e1e69896084233e4f1b4f2e9
hoangphan <hoangphan@asvesa.com>
2022-10-27 15:13:02 +0700

    update port

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
